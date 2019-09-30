032eefaa5063fa954911533762367b5fd4001089
AlexChavez235 <achavz235@gmail.com>
2019-09-30 22:50:53 +0000

    update db env var

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect('mongodb+srv://achavz97:Alexturtle1@cluster0-d4i6e.mongodb.net/test?retryWrites=true&w=majority', {
+
+console.log(process.env.DATABASEURL);
+//"mongodb://localhost:27017/yelp_camp"
+//'mongodb+srv://achavz97:Alexturtle1@cluster0-d4i6e.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(process.env.DATABASEURL, {
 	useNewUrlParser: true,
 	useCreateIndex: true
-}).then(() => {
-	console.log('Connected to DB!');
-}).catch(err => {
-	console.log('ERROR:', err.message);
 });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
